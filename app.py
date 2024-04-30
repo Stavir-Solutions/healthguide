89e453431db687ad97c6b4cd85cd1f5ed4bd29ca
krishnastavir <krishnavamsi@stavir.com>
2024-04-30 11:49:59 +0530

    cognito login & logout button

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
 from flask_awscognito import AWSCognitoAuthentication
-from flask import session
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, set_access_cookies
+import logging
+import requests
+from jwt.algorithms import RSAAlgorithm
  
 
 import logging
@@ -13,7 +17,7 @@ app = Flask(__name__)
 
 logging.basicConfig(filename='application.log', level=logging.DEBUG)
 
-app = Flask(__name__)
+
 app.config["AWS_DEFAULT_REGION"] = "us-east-1"
 app.config["AWS_COGNITO_DOMAIN"] = "https://lifestyle-advise.auth.us-east-1.amazoncognito.com"
 app.config["AWS_COGNITO_USER_POOL_ID"] = "us-east-1_9KNLru65q"
@@ -21,15 +25,18 @@ app.config["AWS_COGNITO_USER_POOL_CLIENT_ID"] = "3vve5j1t85tjp6lrheibta9eka"
 app.config["AWS_COGNITO_REDIRECT_URL"] = "http://localhost:5000"
 app.config["AWS_COGNITO_LOGOUT_URL"] = "https://lifestyle-advise.auth.us-east-1.amazoncognito.com"
 app.config["AWS_COGNITO_USER_POOL_CLIENT_SECRET"] = "abdpbt9vjuuqn7rsh57mod3u6a3m244feu0cg0aoflkerkvknlj"
+app.config["JWT_PUBLIC_KEY"] = RSAAlgorithm.from_jwk
 
 
+CORS(app)
 aws_auth = AWSCognitoAuthentication(app)
+jwt = JWTManager(app)
 
 
 
 @app.route('/')
 def home():
-    return render_template('form.html')
+     return render_template('form.html')
 
 
 @app.route('/submit', methods=['POST'])
@@ -52,13 +59,21 @@ def login():
 def logged_in():
     logging.info("Logged in called")
     access_token = aws_auth.get_access_token(request.args)
+    set_access_cookies(resp, access_token, max_age=30 * 60)
     if access_token:
         session["access_token"] = access_token
+        session["logged_in"] = True
         return redirect(url_for("/"))
         #TODO in the form if user is logged in show logout instead of login
     else:
         # Handle failed login
         return redirect(url_for("login"))
+    
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))   
 
 
 
