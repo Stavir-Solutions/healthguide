ff4ec7d66df6ab43c1d4a1dc6c3bacca727a0409
skStavir <santhosh.krishna@stavir.com>
2024-05-09 09:39:12 +0530

    corrected some logic and added todos

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, session, make_response
 from flask_awscognito import AWSCognitoAuthentication
 from flask_cors import CORS
-from flask import jsonify
-from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
-import logging
-import requests
-from jwt.algorithms import RSAAlgorithm
- 
+from flask_jwt_extended import JWTManager, get_jwt_identity
+
 
 import logging
-import os
 
 from openapi_client import query_openapi
 from query_builder import build_open_api_prompt_from_user_input
 
 app = Flask(__name__)
 
-
 logging.basicConfig(filename='application.log', level=logging.DEBUG)
 
-
+#TODO move the secrets to github secret and access as env variables
 app.config["AWS_DEFAULT_REGION"] = "us-east-1"
 app.config["AWS_COGNITO_DOMAIN"] = "https://lifestyle-advise.auth.us-east-1.amazoncognito.com"
 app.config["AWS_COGNITO_USER_POOL_ID"] = "us-east-1_9KNLru65q"
 app.config["AWS_COGNITO_USER_POOL_CLIENT_ID"] = "3vve5j1t85tjp6lrheibta9eka"
 app.config["SECRET_KEY"] = "AKIAYRUCY23SC7U2YXCC"
+#TODO it should be an evironment configuration
 app.config["AWS_COGNITO_REDIRECT_URL"] = "http://localhost:5000/loggedin"
 app.config["AWS_COGNITO_LOGOUT_URL"] = "http://localhost:5000"
 app.config["AWS_COGNITO_USER_POOL_CLIENT_SECRET"] = "abdpbt9vjuuqn7rsh57mod3u6a3m244feu0cg0aoflkerkvknlj"
@@ -50,22 +45,24 @@ jwt = JWTManager(app)
 
 @app.route('/')
 def home():
-    access_token = session.get("access_token")
     logged_in = False
-    
+    access_token = session.get("access_token")
+
     if access_token:
         try:
             # Validate the access token
-            get_jwt_identity()
-            logged_in = False
+            identity = get_jwt_identity()
+            if identity is None:
+                logging.info("User is token is not valid (identity is none")
+                logged_in = False
+            else:
+                logging.info(f"User identity {identity}")
+                logged_in = True
         except:
             # Token is invalid
-            logged_in = True
+            logged_in = False
     
     return render_template('form.html', logged_in=logged_in)
-
-
-
 
 
 @app.route('/submit', methods=['POST'])
@@ -90,28 +87,19 @@ def logged_in():
     access_token = aws_auth.get_access_token(request.args)
     if access_token:
         session["access_token"] = access_token
+        # TODO once the login code is working try commenting it
         session["logged_in"] = True
         return redirect(url_for("home"))
-        #TODO in the form if user is logged in show logout instead of login
     else:
         # Handle failed login
         logging.error("access_token is not generated")
         return redirect(url_for("home"))
     
-    
-
-    
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
-    # session.pop("access_token", None)  # Remove access token from session
     session.clear()  # Clear Flask session
-    # Redirect to Cognito logout URL to clear the Cognito session
-    return redirect(app.config["AWS_COGNITO_LOGOUT_URL"])   
-
-
-
- 
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
